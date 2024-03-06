ae8aa83656110e5b6bc31992cff7f57605a90528
Riddy366 <ridwanabukar851@gmail.com>
2024-03-06 18:48:40 +0100

    la til routing og api

--- Category_blogg/src/App.jsx
+++ Category_blogg/src/App.jsx
@@ -13,13 +13,14 @@ function App() {
 
   const [content, setContent] = useState([])
   const [query, setQuery] = useState("beth")
+  const [currentId, setCurrentId] = useState(null)
 
   const getData = async()=>{
     try{
-    const response = await fetch(`https://rickandmortyapi.com/api/character?name=${query}`)
-    const data = await response.json()
-    setContent(data.results)
-    }catch {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`)
+      const data = await response.json()
+      setContent(data.results)
+    }catch{
       console.error("Det har skjedd en feil")
     }
   }
@@ -28,18 +29,15 @@ function App() {
     getData()
   },[query])
 
-  console.log("Q", query)
-
-
-
+  console.log("ID", currentId)
 
   return (
     <Layout>
       <Routes>
         <Route index element={<Home content={content}/>}/>
         <Route path="characters/*" element={<Categories />}>
-          <Route index element={<CategoriesIndex content={content} setQuery={setQuery}/>}/>
-          <Route path=':slug' element={<Category posts={posts} />}/>
+          <Route index element={<CategoriesIndex content={content} setQuery={setQuery} setCurrentId={setCurrentId} />}/>
+          <Route path=':slug' element={<Category posts={posts} currentId={currentId} />}/>
           <Route path=':slug/:postid' element={<PostPage posts={posts} />} />
         </Route>
       </Routes>
--- Category_blogg/src/components/CategoriesIndex.jsx
+++ Category_blogg/src/components/CategoriesIndex.jsx
@@ -1,33 +1,34 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-export default function CategoriesIndex({content, setQuery}){
-
-    console.log("sjekk", content)
-
+export default function CategoriesIndex({content, setQuery, setCurrentId}){
     const [search, setSearch] = useState("")
+    
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e)=>{
         e.preventDefault()
         setQuery(search)
-
     }
 
-    const handleChange = (event) => {
+    const handleChange = (event)=>{
         setSearch(event.target.value)
-}
+    }
 
-    console.log("S", search)
+    const handleClick = (id)=>{
+        setCurrentId(id)
+    }
 
+    console.log("S", content)
+    
     return(
     <>
         <h1>Characters</h1>
-        <label htmlFor="search">Søk etter karakter:</label>
         <form onSubmit={handleSubmit}>
+            <label htmlFor="search">Søk etter karakter:</label>
             <input type="text" id="search" placeholder="Rick..." onChange={handleChange}></input>
-            <input type="submit" value="søk"></input>
+            <input type="submit" value="Søk"></input>
         </form>
         <ul className="category-list">
-            {content?.map(item => <li key={item.id}><Link to={item.name}>{item.name}</Link></li>)}
+            {content?.map(item => <li key={item.id}><Link to={item.name} onClick={()=>handleClick(item.id)}>{item.name}</Link></li>)}
         </ul>
     </>)
 }

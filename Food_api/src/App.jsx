6db81ca00e8122185b3f401af7fea990970b5015
Riddy366 <ridwanabukar851@gmail.com>
2024-03-12 14:05:00 +0100

    localStorage
    Log ut og inn funksjon og localStorage

--- Food_api/src/App.jsx
+++ Food_api/src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect } from 'react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
 
 function App() {
+  const [logedIn, setLogedIn] = useState(()=>{
+    const data = localStorage.getItem("logedIn")
+    const logedInData = JSON.parse(data)
+    return logedInData || "" 
+  })
+  const [food, setFood] = useState("");
+  const apiKey = process.env.REACT_APP_API_KEY;
 
-  const [food, setFood] = useState("")
-  const apiKey = process.env.REACT_APP_API_KEY
+  const [users, setUsers] = useState(()=>{
+    const data = localStorage.getItem("users")
+    const userdata = JSON.parse(data)
+    return userdata || ""
+  })
+  console.log(users)
+ 
 
-  const getFood = async()=>{
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=greek`)
-    .then(response => response.json())
-    .then(data => setFood(data.results))
-    .catch(error => console.error(error))
+
+  /*const databaseUsers = [
+    {
+      username: "Riddy",
+      password: "drossap123"
+  },
+  {
+    username: "YSL",
+    password: "drossap321"
   }
+]
 
-  useEffect(() =>{
-    getFood()
-  },[])
+localStorage.setItem("users", JSON.stringify(databaseUsers))*/
 
-  console.log(food)
+  const getFood = async () => {
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=greek`
+    )
+      .then((response) => response.json())
+      .then((data) => setFood(data.results))
+      .catch((error) => console.error(error));
+  };
 
-return (
+  useEffect(() => {
+    getFood();
+  }, []);
+
+  console.log(food);
+
+  return (
     <>
-    <h1>Hello World</h1>
-     
+      <Layout logedIn={logedIn} setLogedIn={setLogedIn}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login setLogedIn={setLogedIn} logedIn={logedIn} users={users} />} />
+        </Routes>
+      </Layout>
+      {!logedIn ? <Navigate to="login" replace /> : <Navigate to="/" replace />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
